e71f15fcd758a3f6e9b0dc67ce7f606dc416863b
bbankit <ankit.pahuja@bigbasket.com>
2019-02-28 12:54:08 +0530

    My first commit

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ankit/Desktop/Learning Development/Local Library Mdn/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
